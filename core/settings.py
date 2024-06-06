61c91f1c5d5fd8420df62af9a6d73a0128d7c969
mrcerverus <mr.arielvc@gmail.com>
2024-06-06 19:59:03 -0400

    carga render1

--- core/settings.py
+++ core/settings.py
@@ -98,7 +98,7 @@ DATABASES = {
         'NAME': os.environ.get('DATABASE_NAME'),
         'USER': os.environ.get('DATABASE_USER'),
         'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
-        'HOST': '127.0.0.1',
+        'HOST': os.environ.get('HOST_NAME'),
         'PORT': '5432',
     }
 }
